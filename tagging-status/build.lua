46caa5a3c41d5712d6a0963e490b4d7b57505baf
mbertucci47 <61854785+mbertucci47@users.noreply.github.com>
2025-10-25 14:27:48 -0700

    woops parentheses

--- tagging-status/build.lua
+++ tagging-status/build.lua
@@ -134,7 +134,7 @@ function find_tasks(name)
     task = line:match"^%% *tasks: (.*)$"
     if task then
       if
-        (task:match("^bibtex") or task:match("^biber") or task:match("^makeindex")) or task:match("^mf")) or task:match("^mpost"))
+        (task:match("^bibtex") or task:match("^biber") or task:match("^makeindex") or task:match("^mf") or task:match("^mpost"))
         and
         not(task:match("[;&<>]"))
       then
